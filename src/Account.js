053c53ab44736b2801a9c7febcb5fb4fd0989566
njang <neo.w.jang@gmail.com>
2018-01-29 16:56:40 -0600

    Complete the all bonuses except transfer

--- src/Account.js
+++ src/Account.js
@@ -7,35 +7,26 @@ export default class Account extends Component {
       balance: 10.0
     }
   }
-  handleDepositClick(e) {
+  handleButtonClick(e, operator) {
     // It is good practice to still prevent default behavior
     e.preventDefault();
     // set a local variable to the amount entered in the text box.
-    let amount = Math.round(parseFloat(this.inputBox.value) * 100)/100;
+    let amount = Number(this.inputBox.value);
+    !isNaN(amount) ? amount = Math.round(parseFloat(this.inputBox.value) * 100)/100 : amount = 0;
     // set a local variable to the new balance based off of the original balance + amount
-    let newBalance = this.state.balance + amount;
+    let newBalance = this.state.balance
+    switch (operator) {
+      case "Deposit":
+        newBalance += amount;
+        break;
+      case "Withdraw":
+        newBalance -= amount;
+        break;        
+    }
     // set the balance to the newBalance using the setState method (necessary)
     this.setState({
       balance: newBalance
     })
-    // empty out the text box in this component
-    this.inputBox.value = '';
-  }  
-  handleWithdrawClick(e) {
-    // It is good practice to still prevent default behavior
-    e.preventDefault();
-    // set a local variable to the amount entered in the text box.
-    let amount = Math.round(parseFloat(this.inputBox.value) * 100)/100;
-    // set a local variable to the new balance based off of the original balance + amount
-    let newBalance = this.state.balance - amount;
-    // set the balance to the newBalance using the setState method (necessary)
-    if (newBalance >= 0) {
-      this.setState({
-        balance: newBalance
-      })
-    } else {
-      alert("Insufficient fund!");
-    }
     // empty out the text box in this component
     this.inputBox.value = '';
   }
@@ -45,8 +36,8 @@ export default class Account extends Component {
         <h2>{this.props.name}</h2>
         <div className="balance">${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
-        <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)} />
-        <input type="button" value="Withdraw" onClick={(e) => this.handleWithdrawClick(e)} />
+        <input type="button" value="Deposit" onClick={(e) => this.handleButtonClick(e, "Deposit")} />
+        <input type="button" value="Withdraw" onClick={(e) => this.handleButtonClick(e, "Withdraw")} />
       </div>
     )
   }
